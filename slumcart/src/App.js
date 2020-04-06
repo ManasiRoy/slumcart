4b840192d70e758f14d2a38c162658b485049529
Manasi Roy <manasiry3@gmail.com>
2020-04-06 19:12:50 +0530

    header add and styling

--- slumcart/src/App.js
+++ slumcart/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Homepage from './Pages/Homepage/homepage';
 import Shop from './Components/Shop/Shop'
-import { BrowserRouter as Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route path='/' exact component={Homepage} />
+        <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={Shop} />
       </Switch>
     </div>
